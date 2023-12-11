a791393d2bf15dffe316d1f8317e731954e5ad19
Raven <ravenlimzhexuan1@gmail.com>
2023-12-11 16:53:26 +0800

    Vector2 standard values

--- src/Structs/Vector2.h
+++ src/Structs/Vector2.h
@@ -33,6 +33,18 @@ namespace Perch
 		PERCH_API Vector2(float x, float y);
 		PERCH_API Vector2(float val);
 
+		PERCH_API inline static Vector2 Zero() { return Vector2(); }
+		PERCH_API inline static Vector2 One() { return Vector2(1.f, 1.f); }
+		PERCH_API inline static Vector2 Half() { return Vector2(0.5f, 0.5f); }
+		PERCH_API inline static Vector2 Left() { return Vector2(0.f, 0.5f); }
+		PERCH_API inline static Vector2 Right() { return Vector2(1.f, 0.5f); }
+		PERCH_API inline static Vector2 Up() { return Vector2(0.5f, 0.0f); }
+		PERCH_API inline static Vector2 Down() { return Vector2(0.5f, 1.0f); }
+		PERCH_API inline static Vector2 TopLeft() { return Vector2(); }
+		PERCH_API inline static Vector2 TopRight() { return Vector2(1.f, 0.f); }
+		PERCH_API inline static Vector2 BottomLeft() { return Vector2(0.f, 1.f); }
+		PERCH_API inline static Vector2 BottomRight() { return Vector2(1.f, 1.f); }
+
 		PERCH_API float Magnitude() const;
 
 		PERCH_API float Dot(Vector2 const& other) const;
