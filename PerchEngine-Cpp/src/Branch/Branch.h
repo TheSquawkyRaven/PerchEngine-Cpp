f1d70df94e59ba0f60c182d2c354f770669cbd13
Raven <ravenlimzhexuan1@gmail.com>
2023-12-13 21:24:28 +0800

    Documentation for Engine and Branch

--- PerchEngine-Cpp/src/Branch/Branch.h
+++ PerchEngine-Cpp/src/Branch/Branch.h
@@ -18,8 +18,9 @@ namespace Perch
 {
 
 	/// <summary>
-	/// A Branch is a container that can hold data and other branches as children.
-	/// This is similar to Godot's Node.
+	/// Branch
+	/// <para>An object that can be attached as a parent of, or/and as a child of another branch object.</para>
+	/// <para>Anything that requires a behaviour needs to be extended from a Branch</para>
 	/// </summary>
 	class Branch
 	{
@@ -36,10 +37,30 @@ namespace Perch
 
 	public:
 
+		/// <summary>
+		/// Sets the name of the branch.
+		/// </summary>
+		/// <param name="name"></param>
 		PERCH_API inline void SetName(std::string name) { this->name = name; }
+		/// <summary>
+		/// Returns the name of the branch.
+		/// </summary>
+		/// <param name="name">Branch name</param>
 		PERCH_API inline std::string GetName() const { return name; }
+		/// <summary>
+		/// Activate or deactivate the branch. Stops many default behaviour from being executed. Affects its children
+		/// </summary>
+		/// <param name="active">Do activate or not</param>
 		PERCH_API inline void SetActive(bool active) { this->active = active; }
+		/// <summary>
+		/// Returns if the branch is active.
+		/// </summary>
+		/// <returns>Branch is active</returns>
 		PERCH_API inline bool IsActive() const { return active; }
+		/// <summary>
+		/// Returns if the branch is globally active, by recursively checking the parent up to its root if any are not active
+		/// </summary>
+		/// <returns>Branch is globally active</returns>
 		PERCH_API bool IsGloballyActive() const;
 
 	private:
@@ -65,9 +86,13 @@ namespace Perch
 
 	public:
 
-		// Ignores self's draw layer if true
+		/// <summary>
+		/// Should inherit parent's draw layer. Will ignore self's draw layer if true
+		/// </summary>
 		bool inheritDrawLayer = true;
-		// Only works if inheritDrawLayer is true. This draw order will be added to parent's draw order.
+		/// <summary>
+		/// Should the draw order be relative. Only works if inheritDrawLayer is true. This draw order will be added to parent's draw order.
+		/// </summary>
 		bool relativeDrawOrder = true;
 
 		// ###
@@ -96,51 +121,135 @@ namespace Perch
 
 	public:
 
+		/// <summary>
+		/// Creates a branch.
+		/// </summary>
+		/// <param name="engine">Engine reference</param>
+		/// <returns>Branch</returns>
 		PERCH_API Branch(Engine* engine);
 
+		/// <summary>
+		/// Attaches a child to this branch.
+		/// <para>Will own the branch using unique_ptr.</para>
+		/// </summary>
+		/// <param name="branch">The child branch to attach</param>
 		PERCH_API void AttachChild(std::unique_ptr<Branch> branch);
-		// Implicitly converts branch into a unique_ptr
+		/// <summary>
+		/// Attaches a child to this branch.
+		/// <para>Shorthand function to implicitly convert the branch to a unique_ptr.</para>
+		/// </summary>
+		/// <param name="branch">The child branch to attach</param>
 		PERCH_API void AttachChildu(Branch* branch);
 
+		/// <summary>
+		/// Attaches a script to this branch.
+		/// <para>Will own the script using unique_ptr.</para>
+		/// </summary>
+		/// <param name="script">The script to attach.</param>
 		PERCH_API void AttachScript(std::unique_ptr<Script> script);
-		// Implicitly converts script into a unique_ptr
+		/// <summary>
+		/// Attaches a script to this branch.
+		/// <para>Shorthand function to implicitly convert the script to a unique_ptr.</para>
+		/// </summary>
+		/// <param name="script">The script to attach</param>
 		PERCH_API void AttachScriptu(Script* script);
 
-		// Ready - Preorder, Called upon attaching to a branch of the main tree or when the tree is run. Only called once. Active independent
+		/// <summary>
+		/// Ready - Called upon attaching to a branch of the main tree or when the tree is run. Only called once.
+		/// <para>Unaffected by the branch's active state.</para>
+		/// <para>Preorder call order to its children.</para>
+		/// <para>Currently broken. Currently only executes the first time after OnRootCreate on the first run of the engine.</para>
+		/// </summary>
 		PERCH_API virtual void Ready();
 
-		// Update - Preorder, Called every frame. Active dependent
+		/// <summary>
+		/// Update - Called every frame.
+		/// <para>Affected by the branch's active state.</para>
+		/// <para>Preorder call to its children.</para>
+		/// </summary>
 		PERCH_API virtual void Update();
-		// UpdateOut - Called every frame after Updating all children. Active dependent
+		/// <summary>
+		/// UpdateOut - Called every frame after updating all children.
+		/// <para>Affected by the branch's active state.</para>
+		/// <para>Postorder call to its children.</para>
+		/// </summary>
 		PERCH_API virtual void UpdateOut();
 
-		// PhysicsUpdate - Preorder, Called every frame after update, before collision update. Active dependent
+		/// <summary>
+		/// PhysicsUpdate - Called every frame after update for physics purposes.
+		/// <para>Affected by the branch's active state.</para>
+		/// <para>Preorder call to its children.</para>
+		/// </summary>
 		PERCH_API virtual void PhysicsUpdate();
-		// CollisionUpdate - Preorder, Called every frame after physics update, before draw. Active dependent
+		/// <summary>
+		/// CollisionUpdate - Called every frame after physics update.
+		/// <para>Affected by the branch's active state.</para>
+		/// <para>Preorder call to its children.</para>
+		/// </summary>
 		PERCH_API virtual void CollisionUpdate();
 
+		/// <summary>
+		/// A default draw order behaviour.
+		/// </summary>
+		/// <param name="renderer">Renderer reference.</param>
 		PERCH_API void SetupDefaultDrawOrder(Renderer* renderer);
-		// SetupDraw - Preorder, Called every frame. Update first, then SetupDraw. Active dependent
+		/// <summary>
+		/// SetupDraw - Called every frame after collision update. Setups the draw order of the branch
+		/// <para>Affected by the branch's active state</para>
+		/// <para>Preorder call to its children</para>
+		/// </summary>
+		/// <param name="renderer">Renderer reference</param>
 		PERCH_API virtual void SetupDraw(Renderer* renderer);
-		// Returns the parent's draw layer if inheritDrawLayer is true
+		/// <summary>
+		/// Returns the global draw layer of this branch.
+		/// <para>Returns the parent's draw layer if inheritDrawLayer is true.</para>
+		/// </summary>
+		/// <returns>Draw Layer</returns>
 		PERCH_API virtual int GetDrawLayer();
-		// Returns this draw order's + parent's draw order relativeDrawOrder is false, otherwise returns this draw order.
-		PERCH_API virtual int GetDrawOrder();
-		// Sets inheritDrawLayer to true or false if it equals the parent's draw layer
+		// 
+
+		/// <summary>
+		/// Returns the global draw order of this branch.
+		/// <para>Returns this draw order's + parent's draw order if relativeDrawOrder is false, otherwise returns this draw order.</para>
+		/// </summary>
+		/// <returns></returns>
+		PERCH_API virtual int GetDrawOrder();		
+		/// <summary>
+		/// Sets the draw layer of the branch.
+		/// <para>Will set inheritDrawLayer to false if is not the same as the parent's.</para>
+		/// </summary>
+		/// <param name="drawLayer">Draw Layer</param>
 		PERCH_API virtual void SetDrawLayer(int drawLayer);
+		/// <summary>
+		/// Sets the draw layer of the branch.
+		/// </summary>
+		/// <param name="drawOrder">Draw Order</param>
 		PERCH_API virtual void SetDrawOrder(int drawOrder);
 
 		void _Draw(Renderer* renderer);
-		// Draw - Order dependent on SetupDraw. SetupDraw must be overriden to setup the draw layer and order.
+		/// <summary>
+		/// Draw - Called after setup draw by the Renderer.
+		/// <para>Draw anything here, that respects the draw layer and draw order.</para>
+		/// <para>Call order depends on Setup Draw</para>
+		/// </summary>
+		/// <param name="renderer">Renderer reference</param>
 		PERCH_API virtual void Draw(Renderer* renderer);
-		void _DrawOut(Renderer* renderer);
+		//void _DrawOut(Renderer* renderer);
 		// DrawOut - Called every frame right after Drawing all children. Active dependent
-		PERCH_API virtual void DrawOut(Renderer* renderer);
+		//PERCH_API virtual void DrawOut(Renderer* renderer);
 
-		// Destroy - Starts the destruction of children and this. Don't use or move any of the children of this after calling Destroy on this branch
+		/// <summary>
+		/// Destroy - Mark this branch and its children for destruction after an update loop has completed.
+		/// <para>Don't use or move any of the children of this after calling Destroy on this branch.</para>
+		/// </summary>
 		PERCH_API void Destroy();
-
-		// OnDestroy - POSTorder, Called while destroying. Uninitialize pointers here. Active independent
+				
+		/// <summary>
+		/// OnDestroy - Executes when the branch is being destroyed.
+		/// <para>Uninitialize manual pointers here</para>
+		/// <para>Postorder call to its children.</para>
+		/// </summary>
+		/// <returns></returns>
 		PERCH_API virtual void OnDestroy();
 
 		// ###
